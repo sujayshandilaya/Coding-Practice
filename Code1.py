d715b9e02d7222d280eaacf1d8910d1aaf8d28a8
Sujay Shandilaya <shandilayasujay@gmail.com>
2021-12-10 00:17:07 -0600

    Leetcode Solution 1

---
+++ Code1.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+       
+        
+        n=len(nums)
+        ans = [None] * 2*n
+        for i in range(n):
+            ans[i]=nums[i]
+            ans[i+n]=nums[i]
+        return(ans)
+            
