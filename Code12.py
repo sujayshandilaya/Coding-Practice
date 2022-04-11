4da96b14a5f01a8e93c8e13e5b0fb06e25303266
Sujay Shandilaya <shandilayasujay@gmail.com>
2022-04-10 22:17:27 -0500

    Update Code12.py

--- Code12.py
+++ Code12.py
@@ -20,4 +20,16 @@ class Solution:
                 nums[x]=i
                 x+=1
         return x
-                
+##############
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i=0
+        j=len(nums)-1
+        while i<=j:
+            if nums[i]==val:
+                nums[i],nums[j]=nums[j], nums[i]
+                j-=1
+            else:
+                i+=1
+        return i
