01933537772ed48dd207882ac5262fdb57f8e226
Sujay Shandilaya <shandilayasujay@gmail.com>
2022-07-22 06:37:05 -0500

    Update Code46.py

--- Code46.py
+++ Code46.py
@@ -40,6 +40,34 @@ class Solution:
                 if nums[j] in num_dict:
                     op.add(tuple(sorted([nums[i],nums[j],num_dict[nums[j]]])))
                     
+###########
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        n=len(nums)
+        op=[]
+        for i in range(n-2):
+            if nums[i]>0: break
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            l=i+1
+            r=n-1
+            while l<r:
+                temp=nums[i]+nums[l]+nums[r]
+                if temp>0:r-=1
+                elif temp<0: l+=1
+                else: 
+                    op.append([nums[i],nums[l],nums[r]])
+                    while l<r and nums[l]==nums[l+1]: l+=1
+                    while l<r and nums[r]==nums[r-1]: r-=1
+                    l+=1
+                    r-=1
+        return op
+
+        
+                    
+                    
                 else:
                     num_dict[-(nums[i]+nums[j])]=nums[j]
         return op
