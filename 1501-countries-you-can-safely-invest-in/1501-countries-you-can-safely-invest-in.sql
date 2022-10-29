7d6656038e80df8ae77a1180fd3d743ca25dd1d3
Sujay Shandilaya <shandilayasujay@gmail.com>
2022-10-28 23:49:07 -0500

    Time: 1873 ms (28.35%), Space: 0B (100.00%) - LeetHub

---
+++ 1501-countries-you-can-safely-invest-in/1501-countries-you-can-safely-invest-in.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+
+Select c.name as country
+from
+(Select Caller_id as caller, duration from Calls
+union all
+Select callee_id as caller, duration from calls) t1
+inner join
+Person p
+on p.id=t1.caller
+inner join Country c
+on left(phone_number,3) = c.country_code
+group by 1
+having avg(duration) > (Select avg(duration) from Calls)
+
