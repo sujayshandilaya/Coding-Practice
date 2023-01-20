c2ce9b55da07d9914abcf0f66d90c28dabd93379
Sujay Shandilaya <shandilayasujay@gmail.com>
2023-01-19 21:04:43 -0600

    Update Code11.py

--- Code11.py
+++ Code11.py
@@ -21,3 +21,20 @@ class Solution:
                 x+=1
         
         return x
+    
+    ###############################
+    
+    class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+        i,j=0,0
+        n=len(nums)
+    
+        while j < n:
+            while j < n and nums[i]==nums[j]:
+                j+=1
+            if j<n:
+                i+=1
+                nums[i],nums[j]=nums[j],nums[i]
+                j+=1
+        return (i+1)
